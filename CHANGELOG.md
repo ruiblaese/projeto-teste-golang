6b36c504be600a1491532dedc4698efd73cfcd34
ruiblaese <ruiblaese@gmail.com>
2021-01-10 21:33:09 -0300

    new features and enhancements

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# ChangeLog
+
+## v0.0.2
+
+### FEATURES
+  * add index page for list pending requests
+
+### ENHANCEMENTS  
+  * add uuid for requisition control
+
+## v0.0.2
+
+### FEATURES
+  * created project
+  * first test of waiting for processing to release requisition
--- main.go
+++ main.go
@@ -2,19 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	ob "github.com/funkygao/golib/observer"
 	"github.com/gin-gonic/gin"
+	"github.com/gofrs/uuid"
 )
+
+var listRequests = []string{}
 
 func main() {
 
 	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/", func(c *gin.Context) {
+
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"DataFields": listRequests,
+		})
+
+	})
 
 	router.GET("/request", func(c *gin.Context) {
 
-		requestId := c.DefaultQuery("id", "1")
+		uuid := uuid.Must(uuid.NewV4())
+		requestId := fmt.Sprint(uuid)
+		log.Print("/request:", requestId)
+		listRequests = append(listRequests, requestId)
 
 		eventCh1 := make(chan interface{})
 		ob.Subscribe(requestId, eventCh1)
@@ -29,11 +45,28 @@ func main() {
 	router.GET("/release-request", func(c *gin.Context) {
 
 		requestId := c.DefaultQuery("id", "1")
+		indice := indexOf(requestId, listRequests)
+		if indice >= 0 {
 
-		ob.Publish(requestId, requestId)
-		c.String(http.StatusOK, "OK")
+			ob.Publish(requestId, requestId)
+			listRequests = append(listRequests[:indice], listRequests[indice+1:]...)
+			c.String(http.StatusOK, "OK")
+
+		} else {
+			c.String(http.StatusOK, "Error")
+		}
+
 	})
 
 	router.Run(":8080")
 
+}
+
+func indexOf(element string, data []string) int {
+	for k, v := range data {
+		if element == v {
+			return k
+		}
+	}
+	return -1
 }
